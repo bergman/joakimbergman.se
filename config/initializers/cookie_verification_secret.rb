ec934600c6815667bb5e0ef0e176aa2271b241b8
Joakim Bergman <joab@kth.se>
2010-03-08 01:02:31 +0100

    Rails 3.0

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '02c39bf7ad787eba7ce54ed555613f2cfe6b62cc5ef642bf84d60ac313b668e2657f683c7bfe9b4b74afa4dac13681e0f60d823040ee2191c20158ca1b80b79b'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_foo_session',
-  :secret      => 'e13b2ab4bf9f8412e3d4eae60b99b77817951dd4208cbfb664677bc0898c7560fab3084e63344f0f2d0d074dc665cb6cafba87aff8cf431491c0b4e1fa71ced2'
+  :key    => '_joakimbergman.se_session',
+  :secret => '65b0ed4ad1e9116a399041f7af3af3ddcaeeb072b84df6de8a11cafdf8522c4bc208a7ffb3b06306a8d085e9e18dc2e49f92b5f1a735930f7b2a9c19b39096d0'
 }
 
 # Use the database for sessions instead of the cookie-based default,
