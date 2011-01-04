a2dfde5b198da8585b6f931dba20c28a3094decb
Joakim Bergman <joab@kth.se>
2011-01-04 10:12:42 +0100

    Updated deploy script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,20 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_type, :user
+set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+require "bundler/capistrano"
+set :whenever_command, "bundle exec whenever"
+require "whenever/capistrano"
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 set :application, "joakimbergman.se"
 set :repository,  "git@github.com:bergman/joakimbergman.se.git"
 set :scm, :git
-set :deploy_to, "/srv/www/#{application}"
+set :deploy_to, "/var/www/#{application}"
 set :branch, "master"
 set :deploy_via, :remote_cache
-set :git_shallow_clone, 1
 set :use_sudo, false
 
 server "joakimbergman.se", :app, :web, :db, :primary => true
@@ -25,9 +32,6 @@ namespace :deploy do
 end
 
 after "deploy:update_code" do
-end
-
-after "deploy" do
   symlink_config_files
   symlink_database
   get_new_tweet
@@ -38,28 +42,28 @@ end
 desc "Symlink database to new release"
 task :symlink_database, :roles => :app do
   %w(production.sqlite3).each do |file|
-    run "ln -ns #{shared_path}/system/db/#{file} #{current_path}/db/#{file}"
+    run "ln -ns #{shared_path}/system/db/#{file} #{release_path}/db/#{file}"
   end
 end
 
 desc "Symlink shared config files to new release"
 task :symlink_config_files, :roles => :app do
   %w(database.yml).each do |file|
-    run "ln -ns #{shared_path}/system/config/#{file} #{current_path}/config/#{file}"
+    run "ln -ns #{shared_path}/system/config/#{file} #{release_path}/config/#{file}"
   end
 end
 
 desc "Get new tweet"
 task :get_new_tweet, :roles => :app do
-  run "cd #{current_path} && RAILS_ENV=production bundle exec rake twitter:timeline"
+  run "cd #{release_path} && RAILS_ENV=production bundle exec rake twitter:timeline"
 end
 
 desc "lastfm"
 task :lastfm, :roles => :app do
-  run "cd #{current_path} && RAILS_ENV=production bundle exec rake lastfm:artists"
+  run "cd #{release_path} && RAILS_ENV=production bundle exec rake lastfm:artists"
 end
 
 desc "Update the crontab file"
 task :update_crontab, :roles => :app do
-  run "cd #{current_path} && bundle exec whenever --update-crontab #{application}"
+  run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
 end
